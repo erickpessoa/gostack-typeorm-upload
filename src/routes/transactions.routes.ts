d319d1c01a6ad9d6a026f9e07ebe9c7def225200
Erick Pessoa <erickpessoa@websisti.com.br>
2020-04-26 12:07:03 -0300

    Implementado Desafio 6 - Create, List - Category; Create, List e Delete - Transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,22 +1,85 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import Category from '../models/Category';
+
 // import ImportTransactionsService from '../services/ImportTransactionsService';
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface TransactionResponse {
+  id: string;
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: Category;
+  created_at: Date;
+  updated_at: Date;
+}
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+  const transactions: Transaction[] = await transactionRepository.find();
+
+  const balance: Balance = await transactionRepository.getBalance();
+
+  const t = transactions.map(
+    ({
+      id,
+      title,
+      value,
+      type,
+      category,
+      created_at,
+      updated_at,
+    }: TransactionResponse) => {
+      return {
+        id,
+        title,
+        value,
+        type,
+        category,
+        created_at,
+        updated_at,
+      };
+    },
+  );
+
+  return response.json({ transactions: t, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category_id } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category_id,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  deleteTransaction.execute(id);
+
+  return response.status(200).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
